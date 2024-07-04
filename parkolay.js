49bd1a0d376c9eb09c72b748ba1cb40cb6411d96
Md. Maynuddin (Mynul) <76878954+maynul1997@users.noreply.github.com>
2024-07-04 20:38:42 +0600

    search in chrome
    Firstly search will test by test cafe

--- parkolay.js
+++ parkolay.js
@@ -4,8 +4,15 @@ fixture('Getting Started')
 
     test('valid Sign up', async t=> {
 
+        await t.typeText('#gLFyf', 'Learning Test here')
+        await t.click(',gNO89b')
+        await t.wait(2000)
 
     });
 
 
-    
+    test('signUp', async t=> {
+
+
+
+    });
